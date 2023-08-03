6b5fff85170587a85fcf969d8585da6f48fd0c04
jakeapple <jake.apple@packsize.com>
2023-08-03 16:47:28 -0600

    Base setup

---
+++ PotterKataDotNet7/BookStoreCart/Cart.cs
@@ -0,0 +1,5 @@
+﻿namespace BookStoreCart;
+
+public class Cart
+{
+}
---
+++ PotterKataDotNet7/CartTests/CartTests.cs
@@ -0,0 +1,20 @@
+using BookStoreCart;
+
+namespace PotterKataCartTests;
+
+public class CartTests
+{
+    Cart classUnderTest;
+    
+    [SetUp]
+    public void Setup()
+    {
+        classUnderTest = new Cart();
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Fail();
+    }
+}
